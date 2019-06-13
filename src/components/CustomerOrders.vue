ef27da465844ccaf92f7efa65be32e4a0f857d91
pierce <pierceshih@gmail.com>
2019-06-13 22:52:46 +0800

    Add shopcartlist.

--- src/components/CustomerOrders.vue
+++ src/components/CustomerOrders.vue
@@ -6,7 +6,7 @@
         <div class="card border-0 shadow-sm">
           <div
             style="height: 150px; background-size: cover; background-position: center"
-            :style="{backgroundImage:`url(${item.image})`}"
+            :style="{backgroundImage:`url(${item.imageUrl})`}"
           ></div>
           <div class="card-body">
             <span class="badge badge-secondary float-right ml-2">{{item.category}}</span>
@@ -16,9 +16,9 @@
             <p class="card-text">內容</p>
             <div class="d-flex justify-content-between align-items-baseline">
               <!-- <div class="h5">2,800 元</div> -->
-              <del v-if="!item.price" class="h6">原價 {{item.origin_price}} 元</del>
-              <del v-if="item.price" class="h6">原價 {{item.origin_price}} 元</del>
-              <div v-if="item.price" class="h5">現在只要 {{item.price}} 元</div>
+              <del v-if="!item.price" class="h6">原價 {{item.origin_price | currency}} 元</del>
+              <del v-if="item.price" class="h6">原價 {{item.origin_price | currency}} 元</del>
+              <div v-if="item.price" class="h5">現在只要 {{item.price | currency}} 元</div>
             </div>
           </div>
           <div class="card-footer d-flex">
@@ -30,7 +30,11 @@
               <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
               查看更多
             </button>
-            <button type="button" class="btn btn-outline-danger btn-sm ml-auto">
+            <button
+              type="button"
+              class="btn btn-outline-danger btn-sm ml-auto"
+              @click="addToCart(item.id)"
+            >
               <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
               加到購物車
             </button>
@@ -39,6 +43,43 @@
       </div>
     </div>
 
+    <!-- cart 內容 -->
+    <div class="my-5 row justify-content-center">
+      <div class="my-5 row justify-content-center">
+        <table class="table mt-3 mb-5">
+          <thead>
+            <th></th>
+            <th width="250">品名</th>
+            <th>數量</th>
+            <th>單價</th>
+          </thead>
+          <tbody>
+            <tr v-for="item in cart.carts" :key="item.id" v-if="cart.carts">
+              <td class="align-middle">
+                <button type="button" class="btn btn-outline-danger btn-sm">
+                  <i class="far fa-trash-alt"></i>
+                </button>
+              </td>
+              <td class="align-middle">{{item.product.title}}</td>
+              <td class="align-middle">{{item.qty}} / {{item.product.unit}}</td>
+              <td class="align-middle text-right">{{item.final_total}}</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colspan="3" class="text-right">總計</td>
+              <td class="text-right">{{ cart.total }}</td>
+            </tr>
+            <tr>
+              <td colspan="3" class="text-right text-success">折扣價</td>
+              <td class="text-right text-success">{{ cart.final_total }}</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
+
+    <!-- productModal 內容 -->
     <div
       class="modal fade"
       id="productModal"
@@ -56,18 +97,18 @@
             </button>
           </div>
           <div class="modal-body">
-            <img :src="product.image" class="img-fluid" alt>
+            <img :src="product.imageUrl" class="img-fluid" alt>
             <blockquote class="blockquote mt-3">
               <p class="mb-0">{{ product.content }}</p>
               <footer class="blockquote-footer text-right">{{ product.description }}</footer>
             </blockquote>
             <div class="d-flex justify-content-between align-items-baseline">
-              <div class="h4" v-if="!product.price">{{ product.origin_price }} 元</div>
-              <del class="h6" v-if="product.price">原價 {{ product.origin_price }} 元</del>
-              <div class="h4" v-if="product.price">現在只要 {{ product.price }} 元</div>
+              <div class="h4" v-if="!product.price">{{ product.origin_price | currency}} 元</div>
+              <del class="h6" v-if="product.price">原價 {{ product.origin_price | currency }} 元</del>
+              <div class="h4" v-if="product.price">現在只要 {{ product.price | currency}} 元</div>
             </div>
             <select name class="form-control mt-3" v-model="product.num">
-              <option value="1">選購 1 件</option>
+              <option :value="num" v-for="num in 10" :key="num">選購 {{num}} {{product.unit}}</option>
             </select>
           </div>
           <div class="modal-footer">
@@ -78,10 +119,10 @@
             <button
               type="button"
               class="btn btn-primary"
-              @click="addtoCart(product.id, product.num)"
+              @click="addToCart(product.id, product.num)"
             >
-              <!-- <i class="fas fa-spinner fa-spin" v-if="product.id === status.loadingItem"></i>
-              加到購物車-->
+              <i class="fas fa-spinner fa-spin" v-if="product.id === status.loadingItem"></i>
+              加到購物車
             </button>
           </div>
         </div>
@@ -98,6 +139,7 @@ export default {
     return {
       products: [],
       product: {},
+      cart: {},
       status: {
         loadingItem: ""
       },
@@ -105,6 +147,7 @@ export default {
     };
   },
   methods: {
+    // 取得所有商品的資料
     getProducts() {
       const api = `${process.env.APIPATH}/api/${
         process.env.CUSTOMPATH
@@ -117,6 +160,7 @@ export default {
         vm.products = response.data.products;
       });
     },
+    // 取得單一商品的詳細資料
     getProduct(id) {
       const api = `${process.env.APIPATH}/api/${
         process.env.CUSTOMPATH
@@ -129,10 +173,38 @@ export default {
         vm.status.loadingItem = "";
         vm.product = response.data.product;
       });
+    },
+    // 加入購物車
+    addToCart(id, qty = 1) {
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
+      const vm = this;
+      vm.status.loadingItem = id;
+      const cart = {
+        product_id: id,
+        qty: qty
+      };
+      vm.$http.post(api, { data: cart }).then(response => {
+        console.log(response.data);
+        vm.status.loadingItem = "";
+        vm.getCart();
+        $("#productModal").modal("hide");
+      });
+    },
+    // 取得購物車的所有商品資料
+    getCart() {
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
+      const vm = this;
+      vm.isLoading = true;
+      vm.$http.get(api).then(response => {
+        console.log("購物車", response.data);
+        vm.isLoading = false;
+        vm.cart = response.data.data;
+      });
     }
   },
   created() {
     this.getProducts();
+    this.getCart();
   }
 };
 </script>
