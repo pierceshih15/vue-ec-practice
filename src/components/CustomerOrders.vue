cc8d884e88e5991506db5a9927bd31698898b990
pierce <pierceshih@gmail.com>
2019-06-13 23:28:38 +0800

    Add order form.

--- src/components/CustomerOrders.vue
+++ src/components/CustomerOrders.vue
@@ -141,6 +141,77 @@
         </div>
       </div>
     </div>
+
+    <!-- 購物車表單 -->
+    <div class="my-5 row justify-content-center">
+      <form class="col-md-6" @submit.prevent="createOrder">
+        <div class="form-group">
+          <label for="useremail">Email</label>
+          <input
+            type="email"
+            class="form-control"
+            name="email"
+            id="useremail"
+            v-model="form.user.email"
+            placeholder="請輸入 Email"
+            required
+          >
+          <span class="text-danger"></span>
+        </div>
+
+        <div class="form-group">
+          <label for="username">收件人姓名</label>
+          <input
+            type="text"
+            class="form-control"
+            name="name"
+            id="username"
+            v-model="form.user.name"
+            placeholder="輸入姓名"
+          >
+          <span class="text-danger"></span>
+        </div>
+
+        <div class="form-group">
+          <label for="usertel">收件人電話</label>
+          <input
+            type="tel"
+            class="form-control"
+            id="usertel"
+            v-model="form.user.tel"
+            placeholder="請輸入電話"
+          >
+        </div>
+
+        <div class="form-group">
+          <label for="useraddress">收件人地址</label>
+          <input
+            type="text"
+            class="form-control"
+            name="address"
+            id="useraddress"
+            v-model="form.user.address"
+            placeholder="請輸入地址"
+          >
+          <span class="text-danger">地址欄位不得留空</span>
+        </div>
+
+        <div class="form-group">
+          <label for="comment">留言</label>
+          <textarea
+            name
+            id="comment"
+            class="form-control"
+            cols="30"
+            rows="10"
+            v-model="form.message"
+          ></textarea>
+        </div>
+        <div class="text-right">
+          <button class="btn btn-danger">送出訂單</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -157,7 +228,16 @@ export default {
       status: {
         loadingItem: ""
       },
-      isLoading: false
+      isLoading: false,
+      form: {
+        user: {
+          name: "",
+          email: "",
+          tel: "",
+          address: ""
+        },
+        message: ""
+      }
     };
   },
   methods: {
@@ -235,6 +315,16 @@ export default {
       vm.$http.post(api, { data: coupon }).then(response => {
         console.log(response.data);
         vm.getCart();
+        vm.isLoading = false;
+      });
+    },
+    createOrder() {
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/order`;
+      const vm = this;
+      const order = vm.form;
+      vm.isLoading = true;
+      vm.$http.post(api).then(response => {
+        console.log("訂單已建立", response);
         vm.isLoading = false;
       });
     }
