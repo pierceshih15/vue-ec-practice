e46e790cfa1db6e855939740aa1890319307ae28
pierce <pierceshih@gmail.com>
2019-06-13 19:57:07 +0800

    Create get single product feature and modal page.

--- src/components/CustomerOrders.vue
+++ src/components/CustomerOrders.vue
@@ -22,13 +22,66 @@
             </div>
           </div>
           <div class="card-footer d-flex">
-            <button type="button" class="btn btn-outline-secondary btn-sm">
-              <i class="fas fa-spinner fa-spin"></i>
+            <button
+              type="button"
+              class="btn btn-outline-secondary btn-sm"
+              @click="getProduct(item.id)"
+            >
+              <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
               查看更多
             </button>
             <button type="button" class="btn btn-outline-danger btn-sm ml-auto">
-              <i class="fas fa-spinner fa-spin"></i>
+              <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
               加到購物車
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="modal fade"
+      id="productModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">{{ product.title }}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <img :src="product.image" class="img-fluid" alt>
+            <blockquote class="blockquote mt-3">
+              <p class="mb-0">{{ product.content }}</p>
+              <footer class="blockquote-footer text-right">{{ product.description }}</footer>
+            </blockquote>
+            <div class="d-flex justify-content-between align-items-baseline">
+              <div class="h4" v-if="!product.price">{{ product.origin_price }} 元</div>
+              <del class="h6" v-if="product.price">原價 {{ product.origin_price }} 元</del>
+              <div class="h4" v-if="product.price">現在只要 {{ product.price }} 元</div>
+            </div>
+            <select name class="form-control mt-3" v-model="product.num">
+              <option value="1">選購 1 件</option>
+            </select>
+          </div>
+          <div class="modal-footer">
+            <div class="text-muted text-nowrap mr-3">
+              小計
+              <strong>{{ product.num * product.price }}</strong> 元
+            </div>
+            <button
+              type="button"
+              class="btn btn-primary"
+              @click="addtoCart(product.id, product.num)"
+            >
+              <!-- <i class="fas fa-spinner fa-spin" v-if="product.id === status.loadingItem"></i>
+              加到購物車-->
             </button>
           </div>
         </div>
@@ -38,10 +91,16 @@
 </template>
 
 <script>
+import $ from "jquery";
+
 export default {
   data() {
     return {
       products: [],
+      product: {},
+      status: {
+        loadingItem: ""
+      },
       isLoading: false
     };
   },
@@ -56,6 +115,19 @@ export default {
         console.log(response.data);
         vm.isLoading = false;
         vm.products = response.data.products;
+      });
+    },
+    getProduct(id) {
+      const api = `${process.env.APIPATH}/api/${
+        process.env.CUSTOMPATH
+      }/product/${id}`;
+      const vm = this;
+      vm.status.loadingItem = id;
+      vm.$http.get(api).then(response => {
+        console.log(response.data);
+        $("#productModal").modal("show");
+        vm.status.loadingItem = "";
+        vm.product = response.data.product;
       });
     }
   },
